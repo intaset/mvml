13c40c3ea1fae58b8c09bf293c058aa50eb7b86a
administrator@international-aset.com <ICNFAicepr1120>
2018-03-16 15:00:31 -0400

    fixed registration form links

--- _site/registrationform.php
+++ _site/registrationform.php
@@ -43,7 +43,7 @@ $message = 'Dear Colleague,
 
 Thank you for registering for MVML 2018. If you have requested any official letters, please allow up to 5 business days to receive your documents.
 
-If you are an author, please make sure to send us your camera ready version and a signed copyright form via email to info@mvml.org. You can find the copyright form here: www.mvml.org/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
+If you are an author, please make sure to send us your camera ready version and a signed copyright form via email to info@mvml.org. You can find the copyright form here: https://mvml.org/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
 
 You can find your registration details below. If there are any errors in the information you have provided, please write an email to us at registration@mvml.org mentioning the correct information. Please note that you SHOULD NOT refill the form.
 
